9278c3bf6b7163a07eebff4a93f9bdd9e3cad433
Gustavo <gustavo.braga10@outlook.com>
2021-04-19 21:11:42 -0300

    Criado api

---
+++ Client-Register/Client-Register-Api/Controllers/ClientController.cs
@@ -0,0 +1,21 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Client_Register_Api.Controllers
+{
+    [ApiController]
+    [Route("api/v1/client")]
+    public class ClientController : ApiBaseController
+    {
+        public ClientController(IMediator mediator): base(mediator)
+        {
+        }
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            return await CreateResponse(async () => await _mediator.Send(new List<string>()));
+        }
+    }
+}
---
+++ Client-Register/Client-Register-Api/Startup.cs
@@ -0,0 +1,67 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+
+namespace Client_Register_Api
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "Register Client",
+                    Description = "Api created to connect with viaCep and send email in architecture of microsservices",
+                    Version = "v1",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Gustavo Braga",
+                        Email = "gustavo.braga10@outlook.com"
+                    }
+                });
+                c.EnableAnnotations();
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+            });
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
